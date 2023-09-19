d9f63d7369be5f67cfaaee4685f61552d19e6fbe
Simeng Hao <simengha@andrew.cmu.edu>
2023-09-18 23:28:18 -0700

    refactor(user): refactor out user-related logic
    user-related routing and db logic are moved to separate files in favor of better project structure

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const path = require('path');
+const logger = require('morgan');
+
+// Internal modules
+const {User, bannedUsernamesSet} = require('./models/User');
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
 
 const app = express();
 const mongoose = require('mongoose');
-const parser = require('body-parser');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
@@ -30,35 +35,9 @@ db.once("open", function () {
   console.log("DB Connected successfully!");
 });
 
-// DB Schema
-const User = mongoose.model('User',{
-  username : String,
-  password : String,
-});
+// Register routes
+app.use('/users', userRouter);
 
-// A list of all banned usernames
-var bannedUsernamesList = "about access account accounts add address adm admin administration adult advertising affiliate \
-  affiliates ajax analytics android anon anonymous api app apps archive atom auth authentication avatar backup banner banners \
-  bin billing blog blogs board bot bots business chat cache cadastro calendar campaign careers cgi client cliente code comercial \
-  compare config connect contact contest create code compras css dashboard data db design delete demo design designer dev devel dir \
-  directory doc docs domain download downloads edit editor email ecommerce forum forums faq favorite feed feedback flog follow file \
-  files free ftp gadget gadgets games guest group groups help home homepage host hosting hostname html http httpd https hpg info \
-  information image img images imap index invite intranet indice ipad iphone irc java javascript job jobs js knowledgebase log login \
-  logs logout list lists mail mail1 mail2 mail3 mail4 mail5 mailer mailing mx manager marketing master me media message microblog \
-  microblogs mine mp3 msg msn mysql messenger mob mobile movie movies music musicas my name named net network new news newsletter nick \
-  nickname notes noticias ns ns1 ns2 ns3 ns4 old online operator order orders page pager pages panel password perl pic pics photo photos \
-  photoalbum php plugin plugins pop pop3 post postmaster postfix posts profile project projects promo pub public python random register \
-  registration root ruby rss sale sales sample samples script scripts secure send service shop sql signup signin search security settings \
-  setting setup site sites sitemap smtp soporte ssh stage staging start subscribe subdomain suporte support stat static stats status store \
-  stores system tablet tablets tech telnet test test1 test2 test3 teste tests theme themes tmp todo task tasks tools tv talk update upload url \
-  user username usuario usage vendas video videos visitor win ww www www1 www2 www3 www4 www5 www6 www7 wwww wws wwws web webmail website websites \
-  webmaster workshop xxx xpg you yourname yourusername yoursite yourdomain"
-bannedUsernamesList = bannedUsernamesList.split(" ");
-// A set of all banned usernames
-const bannedUsernamesSet = new Set();
-for (let i = 0; i < bannedUsernamesList.length; i++) {
-  bannedUsernamesSet.add(bannedUsernamesList[i].toLowerCase());
-}
 
 /*
 This function validates user login info
@@ -101,31 +80,6 @@ app.get('/validateUserInfo', async function (req, res) {
     return res.send({"status": "success", "code": 5});
   } catch (error) {
       console.log(error);
-  }
-});
-
-/*
-This function creates a new user and stores user info into the DB
-- Input: 
-    username (str)
-    password (str)
-- Output: 
-    A HTTP status code
-*/
-app.post('/createNewUser', async (req, res) => {
-  try{
-    const {username, password} = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({username: username, password: hashedPassword});
-    const newUser = await user.save();
-    res.sendStatus(200);
-  }
-  catch (error){
-    res.sendStatus(500);
-    return console.log('createNewUser Error: ', error);
-  }
-  finally{
-    console.log('User Saved')
   }
 });
 
