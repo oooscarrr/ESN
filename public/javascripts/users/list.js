06b396c0e25184e7ed654b04df04c596c170a971
Simeng Hao <simengha@andrew.cmu.edu>
2023-10-05 10:37:39 -0700

    add dynamic update to directory page

--- public/javascripts/users/list.js
+++ public/javascripts/users/list.js
@@ -11,14 +11,11 @@ const post_load = () => {
       window.location.href = '/';
     });
   });
-
-  // socket.on('userOnlineStatusChanged', function (data) {
-  //   const userId = data.userId;
-  //   const userStatus = data.userStatus;
-  //   const userStatusElement = $(`#userStatus${userId}`);
-  //   userStatusElement.text(userStatus);
-  // });
 };
 
-
 $(document).ready(post_load);
+
+socket.on('onlineStatusUpdate', () => {
+  console.log('onlineStatusUpdate');
+  $("#DirectoryBox").load(location.href + " #DirectoryBox");
+});
