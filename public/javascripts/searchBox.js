9699cb76def9ac03c6c1b428dcc1ad26d4eae5dd
Simeng Hao <simengha@andrew.cmu.edu>
2023-11-05 06:38:14 -0800

    feat(search): WIP: add search box
    - added query box with elements showing/hiding on based on context and category
    - added result box to be used to display search result
    - TODO: figure out how to render content for display (maybe use current templates as a starting point)
    - TODO: add behavior to hide pagination icons in user search
    - TODO: for contexts involving pagination, add frontend element to show and keep track of current page
    - TODO: figure out what to include in criteria in each context
    - TODO: restore private chat search by status functionality
    - TODO: figure our why the grid looks so weird if we have time (probably won't lol)

--- public/javascripts/searchBox.js
+++ public/javascripts/searchBox.js
@@ -1,5 +1,197 @@
 import { CONTEXT, urlToContext } from '/shared/context.js';
 
+/**
+ * 
+ * @returns {CONTEXT} The context of the current page. Will be one of the values in the CONTEXT enum.
+ */
+export const determineContext = () => {
+    var currentPathname = window.location.pathname;
+    return urlToContext(currentPathname);
+}
+
+
+export const initializeSearchBox = () => {
+    console.log('Initializing search box');
+    const context = determineContext();
+    console.log(context);
+    displayAndHideSearchElements(context);
+    addSearchElementBehavior(context);
+
+}
+
+const displayAndHideSearchElements = (context) => {
+    if (context == CONTEXT.CITIZENS) {
+        $('#statusSelect').closest('.ui.dropdown').hide();
+    }
+}
+
+const addSearchElementBehavior = (context) => {
+    addSearchButtonBehavior(context);
+    switch (context) {
+        case CONTEXT.CITIZENS:
+            addCitizensSearchElementsBehavior();
+            break;
+        case CONTEXT.ANNOUNCEMENTS:
+            break;
+        case CONTEXT.PUBLIC_MESSAGES:
+            break;
+        case CONTEXT.PRIVATE_MESSAGES:
+            addPrivateMessagesSearchElementsBehavior();
+            break;
+        default:
+            break;
+    }
+}
+
+const addSearchButtonBehavior = (context) => {
+    $('#searchButton').click(getContextSpecificSearchButtonClickHandler(context));
+}
+
+const getContextSpecificSearchButtonClickHandler = (context) => {
+    return () => {
+        if (!makeSureTextInputIsNotEmpty()) {
+            return;
+        }
+        switch (context) {
+            case CONTEXT.CITIZENS:
+                searchCitizens();
+                break;
+            case CONTEXT.ANNOUNCEMENTS:
+                searchAnnouncements();
+                break;
+            case CONTEXT.PUBLIC_MESSAGES:
+                searchPublicMessages();
+                break;
+            case CONTEXT.PRIVATE_MESSAGES:
+                searchPrivateMessages();
+                break;
+            default:
+                break;
+        }
+
+    }
+}
+
+const makeSureTextInputIsNotEmpty = () => {
+    const input = $('#searchBox .ui.input input');
+    if(input.is(':hidden')){
+        return true;
+    }
+    if (input.val().trim() == '') {
+        input.closest('.ui.input').addClass('error');
+        return false;
+    }
+    input.closest('.ui.input').removeClass('error');
+    return true;
+}
+
+const searchCitizens = () => {
+    const selectedCategory = $('#citizenSearchCategorySelect').val();
+    let criteria;
+    if (selectedCategory == 'status') {
+        criteria = {
+            status: $('#statusSelect').val(),
+        }
+    } else {
+        criteria = {
+            usernameFragment: $('#usernameInput').val(),
+        }
+    }
+    makeSearchRequest(CONTEXT.CITIZENS, criteria, onCitizenSearchSuccess, onSearchError);
+}
+
+const onCitizenSearchSuccess = (response) => {
+    // TODO: Display search results
+    console.log(response);
+
+}
+
+const searchAnnouncements = () => {
+    const criteria = $('#announcementSearchInput').val();
+    makeSearchRequest(CONTEXT.ANNOUNCEMENTS, criteria, onAnnouncementSearchSuccess, onSearchError);
+}
+
+const onAnnouncementSearchSuccess = (response) => {
+    // TODO: Display search results
+
+}
+
+const searchPublicMessages = () => {
+    const criteria = $('#publicMessageSearchInput').val();
+    makeSearchRequest(CONTEXT.PUBLIC_MESSAGES, criteria, onPublicMessageSearchSuccess, onSearchError);
+}
+
+const onPublicMessageSearchSuccess = (response) => {
+    // TODO: Display search results
+
+}
+
+const searchPrivateMessages = () => {
+
+    // const selectedCategory = $('#privateMessageSearchCategorySelect').val();
+    // let criteria;
+    // if (selectedCategory == 'message') {
+    //     criteria = {
+    //         query: $('#messageInput').val(),
+    //     }
+    // }
+    // makeSearchRequest(CONTEXT.PRIVATE_MESSAGES, criteria, onPrivateMessageSearchSuccess, onSearchError);
+}
+
+const onPrivateMessageSearchSuccess = (response) => {
+    // TODO: Display search results
+
+}
+
+const onSearchError = (xhr, status, error) => {
+    console.error('AJAX error:', status, error);
+}
+
+const makeSearchRequest = (context, criteria, onSuccess, onError) => {
+    const query = {context, criteria};
+    $.ajax({
+        method: 'GET',
+        url: '/search',
+        data: query,
+        success: onSuccess,
+        error: onError,
+    });
+}
+
+const addCitizensSearchElementsBehavior = () => {
+    $('#citizenSearchCategorySelect').change(onCitizenSearchCategorySelectChange);
+}
+
+const onCitizenSearchCategorySelectChange = () => {
+    const selectedCategory = $('#citizenSearchCategorySelect').val();
+    if (selectedCategory == 'status') {
+        $('#statusSelect').closest('.ui.dropdown').show();
+        $('#usernameInput').hide();
+    } else {
+        $('#statusSelect').closest('.ui.dropdown').hide();
+        $('#usernameInput').show();
+    }
+
+}
+
+const addPrivateMessagesSearchElementsBehavior = () => {
+    $('#privateMessageSearchCategorySelect').change(onPrivateMessageSearchCategorySelectChange);
+}
+
+const onPrivateMessageSearchCategorySelectChange = () => {
+    const selectedCategory = $('#privateMessageSearchCategorySelect').val();
+    if (selectedCategory == 'message') {
+        $('#messageInput').show();
+    } else {
+        $('#messageInput').hide();
+    }
+}
+
+
+// export function showSearchDropdown(context) {
+//     if (context == CONTEXT.CITIZENS) {
+//         $('#categorySelect').show();
+//     }
 
 //     // Prevent closing the dropdown when interacting with the input and button
 //     $('.ui.action.input').on('click', function (e) {
@@ -22,14 +214,9 @@ import { CONTEXT, urlToContext } from '/shared/context.js';
 //         }
 //     });
 // }
-// function isUserSearch() {
-//     var currentPathname = window.location.pathname;
-//     console.log((currentPathname));
-//     if(currentPathname == '/users'){
-//         return true;
-//     }
-//     return false;
-// }
+
+
+
 // function clearSearchInput() {
 //     $('.ui.action.input input').val('');
 // }
@@ -82,14 +269,10 @@ import { CONTEXT, urlToContext } from '/shared/context.js';
 
 
 // function findContext(currentPathname) {
-//     for (const key in contextDict) {
+//     for (const key in path_to_context) {
 //         if (currentPathname.startsWith(key)) {
-//             return contextDict[key];
+//             return path_to_context[key];
 //         }
 //     }
 //     return 'unknownContext';
 // }
-export const showLoaded = () => {
-    console.log('searchBox.js loaded');
-}
-// console.log('searchBox.js loaded');
