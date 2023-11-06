536f37898d47c5a6c200b7f5343ad52b3f25e383
oooscarrr <ruizhizx@andrew.cmu.edu>
2023-11-06 15:53:20 -0800

    bug fixes

--- public/javascripts/announcement.js
+++ public/javascripts/announcement.js
@@ -1,38 +1,47 @@
 const socket = io.connect();
 
 $(document).ready(function () {
-    // Function to post a new announcement
-    function postAnnouncement() {
-        let announcementContent = $('#announcementInput').val();
-        if (announcementContent.trim() !== '') {
-            $.ajax({
-                method: 'POST',
-                url: '/announcements',
-                data: {
-                    content: announcementContent
-                }
-            }).done(function (response) {
-                $('#announcementInput').val(''); // Clear the input 
-                console.log('Announcement posted successfully');
-            }).fail(function (error) {
-                console.error('Error posting announcement:', error);
-            });
-        }
-    }
-
-    // Event handler for the post button
-    $('#postAnnouncementBtn').click(function () {
-        postAnnouncement();
-    });
+    addElementsBehavior();
 });
 
+// Function to post a new announcement
+function postAnnouncement() {
+    const announcementContent = $('#announcementInput').val();
+    if (announcementContent.trim() !== '') {
+        $.ajax({
+            method: 'POST',
+            url: '/announcements',
+            data: {
+                content: announcementContent
+            }
+        }).done(function (response) {
+            $('#announcementInput').val(''); // Clear the input
+        }).fail(function (error) {
+            console.error('Error posting announcement:', error);
+        });
+    }
+}
+
+function addElementsBehavior() {
+    $('#postAnnouncementBtn').click(postAnnouncement);
+    
+    $('#announcementInput').on('keydown', function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            postAnnouncement();
+        }
+    });
+}  
 
 // Socket event listener for new announcements
 socket.on('newAnnouncement', function (announcement) {
     // append the new announcement
-    $('#announcementsList').append(`<div><strong>${announcement.posterName}</strong>: ${announcement.content}</div>`);
+    $('#announcementsList').prepend(announcementObj(announcement));
 });
 
-
-
-  
+function announcementObj(announcement) {
+    const localTime = new Date(announcement.createdAt).toLocaleString()
+    return "<li class='announcement-item'><div class='announcement-content'><strong>" + announcement.posterName
+        + ":</strong><span>" + announcement.content + "</span></div><div class='announcement-timestamp'><span>"
+        + localTime + "</span></div></li>"
+}
