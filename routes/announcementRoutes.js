c9942e9c3ea01dee5e89668aa01bf90db53068b0
Simeng Hao <simengha@andrew.cmu.edu>
2023-12-02 13:04:24 -0800

    feat(speedtest, announcements): Restrict certain actions to user with appropriate privilege levels
    Entire speed test now requires admin privilege
    Post annoucement now requires coordinator privilege
    
    TODO: maybe remove buttons from view if below required privilege level?

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import {post_new_announcement, list_announcements} from '../controllers/announcementController.js';
 import authorization from '../middlewares/authorization.js';
+import { PrivilegeLevel } from '../models/User.js';
+import { requirePrivilege } from '../middlewares/requirePrivilege.js';
 
 const router = express.Router();
 
 router.use(authorization);
-router.post('', post_new_announcement);
+router.post('', requirePrivilege(PrivilegeLevel.COORDINATOR), post_new_announcement);
 router.get('', list_announcements);
 
 export default router;
