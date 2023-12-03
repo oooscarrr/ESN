7eaec43b3489dbaae821f2a2c5ed6063269c8d60
XavierGong <zizang@andrew.cmu.edu>
2023-12-03 10:41:39 -0800

    add validateUserProfileEdit function code

--- controllers/administerUserProfileController.js
+++ controllers/administerUserProfileController.js
@@ -1,4 +1,4 @@
-import { User } from '../models/User.js';
+import { User, bannedUsernamesSet } from '../models/User.js';
 import { io } from '../app.js';
 
 export const displayUserSelectionPage = async (req, res) => {
@@ -25,8 +25,30 @@ export const displayUserProfileEditPage = async (req, res) => {
 export const validateUserProfileEdit = async (req, res) => {
     const { userId } = req.params;
     const { username, privilege, isActive, plain_password } = req.body;
-    // TODO: perform actual validation (maybe in the model?)
     const validationErrors = [];
+    // TODO: This seems wrong (plain_password is always undefined and isActive is 'on' when choosing 'Inactive')
+    console.log("VALIDATION DATA: ", username, "  ", privilege, "  ", isActive, "  ", plain_password);
+    // Perform validation for username
+    if (username) {
+        const lower_case_username = username.toLowerCase();
+        if (lower_case_username.length < 3) {
+            validationErrors.push("Length of username should be at least 3");
+        }
+        if (bannedUsernamesSet.has(lower_case_username)) {
+            validationErrors.push("Username is banned, try another one");
+        }
+        const user = await User.findByUsername(lower_case_username);
+        if (user) {
+            validationErrors.push("Username is already taken, try another one");
+        }
+    }
+    // Perform validation for password
+    if (plain_password) {
+        if (plain_password.length < 4) {
+            validationErrors.push("Length of password should be at least 4");
+        }
+    }
+    // No validation needed for privilege and isActive
     return res.status(200).json(validationErrors);
 }
 
