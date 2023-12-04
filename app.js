64cf42c7d84b351de40741d8859e11812fb2a448
oooscarrr <ruizhizx@andrew.cmu.edu>
2023-12-04 02:13:14 -0800

    fixed error handling

--- app.js
+++ app.js
@@ -45,18 +45,19 @@ app.set('view engine', 'pug');
 registerRoutes(app);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(handleError);
-
-function handleError(err, req, res) {
+app.use(function (err, req, res) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
-}
+});
 
 export { server, io, app };
