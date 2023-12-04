65bcbca57a0dcf1b57580d1eeea7179b4ed39bbd
oooscarrr <ruizhizx@andrew.cmu.edu>
2023-12-03 20:46:31 -0800

    anohter try

--- socket.js
+++ socket.js
@@ -4,57 +4,68 @@ import jwt from 'jsonwebtoken';
 import { User } from './models/User.js';
 import { change_user_online_status } from './controllers/userController.js';
 
+async function updateUserSocketId(userId, socketId) {
+    try {
+        await User.findByIdAndUpdate(userId, { socketId: socketId });
+    } catch (error) {
+        console.error('Error updating user socketId:', error);
+    }
+}
+
+function parseToken(socket) {
+    const cookies = cookie.parse(socket.handshake.headers.cookie);
+    return cookies.token;
+}
+
+function authenticateToken(token) {
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET_KEY);
+    } catch {
+        return null;
+    }
+}
+
+function handleDisconnection(userId, onlineUsers, socket) {
+    console.log('IO Disconnected by', socket.id);
+    if (userId && onlineUsers[userId]) {
+        onlineUsers[userId]--;
+        if (onlineUsers[userId] === 0) {
+            setTimeout(() => {
+                if (userId && onlineUsers[userId] === 0) {
+                    change_user_online_status(userId, false);
+                    delete onlineUsers[userId];
+                }
+            }, 1000);
+        }
+    }
+}
+
 export default function setUpSocketIO(server) {
     const io = new Server(server);
     const onlineUsers = {};
 
-    async function updateUserSocketId(userId, socketId) {
-        try {
-            await User.findByIdAndUpdate(userId, { socketId: socketId });
-        } catch (error) {
-            console.error('Error updating user socketId:', error);
-        }
-    }
-
     io.on('connection', socket => {
         console.log('IO Connected by', socket.id);
-        const cookies = cookie.parse(socket.handshake.headers.cookie);
-        const token = cookies.token;
-        let userId;
+        const token = parseToken(socket);
+        const data = authenticateToken(token);
+        const userId = data ? data.id : null;
 
-        if (token) {
-            try {
-                const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
-                userId = data.id;
-                if (!onlineUsers[userId]) {
-                    onlineUsers[userId] = 1;
-                    change_user_online_status(userId, true);
-                } else {
-                    onlineUsers[userId]++;
-                }
-            } catch {
-                console.log('Invalid token');
-                socket.disconnect();
-            }
-        }
-        if (userId) {
-            updateUserSocketId(userId, socket.id);
+        if (!userId) {
+            console.log('Invalid token');
+            socket.disconnect();
+            return;
         }
 
-        socket.on('disconnect', () => {
-            console.log('IO Disconnected by', socket.id);
-            if (userId) {
-                onlineUsers[userId]--;
-                if (onlineUsers[userId] === 0) {
-                    setTimeout(() => {
-                        if (userId && onlineUsers[userId] === 0) {
-                            change_user_online_status(userId, false);
-                            delete onlineUsers[userId];
-                        }
-                    }, 1000);
-                }
-            }
-        });
+        if (!onlineUsers[userId]) {
+            onlineUsers[userId] = 1;
+            change_user_online_status(userId, true);
+        } else {
+            onlineUsers[userId]++;
+        }
+        
+        updateUserSocketId(userId, socket.id);
+
+        socket.on('disconnect', () => handleDisconnection(userId, onlineUsers, socket));
     });
 
     return io;
